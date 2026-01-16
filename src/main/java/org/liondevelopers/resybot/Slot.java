35516c933c788169037e698fc72b4586d16c3e10
Andrew Hsu <andrewhsudev@gmail.com>
2026-01-16 07:30:11 -0500

    Add Slot.java

---
+++ src/main/java/org/liondevelopers/resybot/Slot.java
@@ -0,0 +1,13 @@
+public class Slot {
+    public String date;
+    public int partySize;
+    public String time;
+
+    public Slot(String date, int partySize, String time) {
+
+        this.date = date;
+        this.partySize = partySize;
+        this.time = time;
+
+    }
+}
